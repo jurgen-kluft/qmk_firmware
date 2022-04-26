d3bfdb362ab601a055c8fac437848a3ff30850e9
Jurgen <jurgen.kluft@gmail.com>
2022-04-26 10:34:28 +0800

    * debounce = 3

--- keyboards/moonlander/config.h
+++ keyboards/moonlander/config.h
@@ -55,7 +55,7 @@
 #define DIODE_DIRECTION ROW2COL
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
+#define DEBOUNCE 3
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
